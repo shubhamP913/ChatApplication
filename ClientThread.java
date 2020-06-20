38ef64a4c2a3c5c59cf334b9daf3be3262d12217
ShubhAM <littleshubham2694@gmail.com>
2020-06-20 12:33:48 +0530

    UserName Added

--- ClientThread.java
+++ ClientThread.java
@@ -1,36 +1,56 @@
+import java.util.*;
+
 import java.io.*;
 import java.net.*;
 
-public class ClientThread extends Thread
+class ClientThread extends Thread
 {
-    private Socket socket = null;
-    public  ClientThread(Socket socket)
+    private Socket socket;
+    private Server server;
+    private PrintWriter writer;
+    public  ClientThread(Socket socket,Server server)
     {
         this.socket = socket;
+        this.server = server;
     }
     public void run()
     {
         try 
         {
-            PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            out.println("Hello Client");
-            String message;
+            InputStream input = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+ 
+            OutputStream output = socket.getOutputStream();
+            writer = new PrintWriter(output, true);
+
+            String userName = reader.readLine();
+
+            String serverMessage = userName+" connected.";
+            writer.println(serverMessage);
+            String clientMessage;
             do
             {
-                message = in.readLine();
-                System.out.println(message);
+                clientMessage = reader.readLine();
+                server.broadcast("["+userName+"] :"+clientMessage,this);
             }
-            while(message.compareTo("BYE")!=0);
-            in.close();
-            out.close();
+            while(clientMessage.compareTo("logout")!=0);
+
+            serverMessage = userName + " has quitted.";
+            
+            reader.close();
+            writer.close();
             socket.close();
+            server.broadcast(serverMessage,this);
             
         } 
         catch (Exception e) 
         {
             System.out.println("Error in ClientThread : "+e.toString());
         }
+    }
+    void sendMessage(String message)
+    {
+        writer.println(message);
     }
     
 }
