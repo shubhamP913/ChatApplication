1c28f4eb0f9eff5eeb30058394e69961d9edfbb6
ShubhAM <littleshubham2694@gmail.com>
2020-06-20 12:35:16 +0530

    Broadcast function added to send messege to all users

--- Server.java
+++ Server.java
@@ -1,26 +1,45 @@
+
+import java.util.*;
 import java.io.*;
 import java.net.*;
 
 class Server
 {
-    public static void main(String args[])
+    private Set<String> userNames = new HashSet<>();
+    private Set<ClientThread> users = new HashSet<>();
+    static private int port = 6920;
+    static private Scanner in ;
+    public static void main(final String args[]) 
+    {
+        Server ss = new Server();
+        ss.execute();
+    }
+    public void execute()
     {
         try 
         {
-            ServerSocket server = new ServerSocket(6920);
+            ServerSocket server = new ServerSocket(port);
             System.out.println("Waiting for client...");
-            while(true)
-            {        
+            while (true) 
+            {
                 Socket socket = server.accept();
-                ClientThread client = new ClientThread(socket);
-                client.start();
+                System.out.println("New user connected");
+                ClientThread newUser = new ClientThread(socket,this);
+                users.add(newUser);    
+                newUser.start();
             }
-            //server.close();
-        } 
-        catch (Exception e) 
+            // server.close();
+        } catch (final Exception e) 
+        {    
+            System.out.println("Error in Server : "+e.toString());
+        }
+    }
+    void broadcast(String message,ClientThread exclude) 
+    {
+        for (ClientThread aUser : users) 
         {
-            //TODO: handle exception
-            //System.out.println("Error in Server : "+e.toString());
+            if(aUser!=exclude)
+                aUser.sendMessage(message);
         }
     }
 }
